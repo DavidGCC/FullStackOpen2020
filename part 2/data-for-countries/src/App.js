bb604e9d046d6a935c84f194af4de9169c865a28
DavidG <datog309@gmail.com>
2020-10-09 01:22:32 +0400

    shows Georgia

--- part 2/data-for-countries/src/App.js
+++ part 2/data-for-countries/src/App.js
@@ -27,6 +27,9 @@ function App() {
     } else {
         displayToShow = TOO_MANY_SEARCHES;
     }
+    if (query.toLowerCase() === "georgia") {
+        displayToShow = <Country value={filtered} />
+    }
   return (
     <div>
         <h1>Find Country</h1>
