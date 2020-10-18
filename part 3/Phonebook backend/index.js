a79a687d267b0818ec9c72c4fa7e22778397b78c
DavidG <datog309@gmail.com>
2020-10-19 01:34:31 +0400

    fixed eslint problems and finished part 3

--- part 3/Phonebook backend/index.js
+++ part 3/Phonebook backend/index.js
@@ -61,7 +61,7 @@ app.put("/api/persons/:id", (req, res, next) => {
 const errorHandler = (error, req, res, next) => {
     console.error(error.message);
     if (error.name === "CastError") {
-        return res.status(400).send({ error: "Malformed Id" });
+        return res.status(400).send({ error: "Malformatted Id" });
     }
     if (error.name === "ValidationError") {
         res.status(400).send({ error: error.message });
