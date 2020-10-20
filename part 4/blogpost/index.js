d4e94b54b33370a08cfdb3bb7f29eab7ce1b651f
DavidG <datog309@gmail.com>
2020-10-20 22:59:54 +0400

    fixed eslint

--- part 4/blogpost/index.js
+++ part 4/blogpost/index.js
@@ -1,10 +1,10 @@
-const http = require('http')
-const app = require("./app");
-const config = require("./utils/config");
+const http = require('http');
+const app = require('./app');
+const config = require('./utils/config').PORT;
 
 const server = http.createServer(app);
 
 const PORT = config.PORT;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
