52269dd1ae262e36d7ee951ef805295833a009f5
DavidG <datog309@gmail.com>
2020-10-21 02:39:45 +0400

    fixes

--- part 4/blogpost/index.js
+++ part 4/blogpost/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const config = require('./utils/config').PORT;
+const config = require('./utils/config');
 
 const server = http.createServer(app);
 
--- part 4/blogpost/utils/list_helper.js
+++ part 4/blogpost/utils/list_helper.js
@@ -1,5 +1,3 @@
-const e = require("express");
-
 const dummy = (blogs) => {
     return 1;
 };
