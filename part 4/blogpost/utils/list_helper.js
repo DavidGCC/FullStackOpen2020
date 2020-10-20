f29443759b1a198f2ad19a6a609e962b7e5d9701
DavidG <datog309@gmail.com>
2020-10-21 01:00:13 +0400

    mostBlogs test works

--- part 4/blogpost/utils/list_helper.js
+++ part 4/blogpost/utils/list_helper.js
@@ -5,8 +5,26 @@ const dummy = (blogs) => {
 const totalLikes = blogs => blogs.reduce((acc, i) => acc + i.likes, 0);
 const favoriteBlog = blogs => blogs.length > 0 ? blogs.reduce((acc, i) => acc.likes > i.likes ? acc : i) : 0; 
 
+const mostBlogs = blogs => {
+    let counted = {};
+    blogs.map(i => {
+        if (counted[i.author] === undefined) {
+            counted[i.author] = 1;
+        } else {
+            counted[i.author]++;
+        }
+    });
+    let sorted = Object.entries(counted).sort((a, b) => b[1] - a[1]);
+    return {
+        'author': sorted[0][0],
+        'blogs': sorted[0][1]
+    };
+}
+
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
