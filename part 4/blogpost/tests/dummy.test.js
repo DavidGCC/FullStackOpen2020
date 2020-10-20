652392a670e9dc4d1347c0bc2a3541dcfa279ba8
DavidG <datog309@gmail.com>
2020-10-20 23:21:59 +0400

    dummy test works

---
+++ part 4/blogpost/tests/dummy.test.js
@@ -0,0 +1,9 @@
+const { TestScheduler } = require('jest');
+const listHelper = require('../utils/list_helper');
+
+test('dummy returns one', () => {
+    const blogs = [];
+
+    const result = listHelper.dummy(blogs);
+    expect(result).toBe(1);
+});
