feedb624fa496bcdf2a8ed5b076548871cd9c37b
DavidG <datog309@gmail.com>
2020-11-01 23:43:47 +0400

    added functionality to show full blog info

--- part 5/bloglist-frontend/src/components/Blog.js
+++ part 5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,25 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div style={{fontSize: '1.5rem', fontFamily: 'sans-serif'}}>
-    <p><b>{blog.title}</b> by <b>{blog.author}</b></p>
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+    const style = { fontSize: '1.5rem', fontFamily: 'sans-serif', border: '1px solid black', width: 1500, padding: 10, boxSizing: 'border-box', marginBottom: 10 }
+    const buttonStyle = { fontSize: '1rem' }
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisibility = () => setVisible(!visible);
+
+    return (
+        <div style={style}>
+            <p><b>{blog.title}</b> by <b>{blog.author}</b> <button style={buttonStyle} onClick={toggleVisibility}>{visible ? 'Hide' : 'Show'}</button></p>
+            {visible && (
+                <div>
+                    <p>{blog.url}</p>
+                    <p>Likes: {blog.likes} <button style={buttonStyle}>Like</button></p>
+                    <p>Created by User: {blog.user.name}</p>
+                </div>
+            )}
+        </div>
+    )
+}
 
 export default Blog
