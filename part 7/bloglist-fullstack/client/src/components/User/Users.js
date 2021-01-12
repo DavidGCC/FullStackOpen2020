d19814e47e57614df3d7facc21e836fc6af4bd98
DavidG <datog309@gmail.com>
2021-01-13 00:15:19 +0400

    users page style

--- part 7/bloglist-fullstack/client/src/components/User/Users.js
+++ part 7/bloglist-fullstack/client/src/components/User/Users.js
@@ -2,30 +2,34 @@ import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import Link from '@material-ui/core/Link'
 import { useSelector } from 'react-redux'
+import { Typography, Grid, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core'
+
 
 const Users = () => {
     const users = useSelector(state => state.users)
     if (users) {
         return (
-            <table>
-                <caption>Users</caption>
-                <thead>
-                    <tr>
-                        <th>User</th>
-                        <th>Blogs Created</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map(user => {
-                        return (
-                            <tr key={user.id}>
-                                <td><Link component={RouterLink} to={`/users/${user.id}`}>{user.name}</Link></td>
-                                <td>{user.blogs.length}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            <Grid container justify='center'>
+                <TableContainer>
+                    <Typography variant='h3' component='h3'>Users</Typography>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>User</TableCell>
+                            <TableCell>Blogs Created</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {users.map(user => {
+                            return (
+                                <TableRow key={user.id}>
+                                    <TableCell><Link component={RouterLink} to={`/users/${user.id}`}>{user.name}</Link></TableCell>
+                                    <TableCell>{user.blogs.length}</TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </TableContainer>
+            </Grid>
         )
     } else {
         return <h3>Loading...</h3>
