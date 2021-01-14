82c59a646f01811f14e368e3f546d3ef9e11c924
DavidG <datog309@gmail.com>
2021-01-14 17:25:21 +0400

    exerise 8.4

--- part 8/library-backend/index.js
+++ part 8/library-backend/index.js
@@ -96,7 +96,7 @@ const typeDefs = gql`
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks: [Book!]!
+        allBooks(author: String): [Book!]!
         allAuthors: [Author!]!
     }
 `
@@ -105,7 +105,12 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: () => books,
+        allBooks: (root, args) => {
+            if (!args.author) {
+                return books;
+            }
+            return books.filter(book => book.author === args.author)
+        },
         allAuthors: () => authors
     },
     Author: {
